a9cf1cb6afeaeb770700dbfb3ce8ec50805f6a54
kirti <kirti25032007@gmail.com>
2025-01-02 12:54:43 +0530

    backend structure

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+
+app.post("/user/signup", function (req, res) {
+  res.json({
+    message: "signup endpoint",
+  });
+});
+app.post("/user/signin", function (req, res) {
+  res.json({
+    message: "signin endpoint",
+  });
+});
+app.get("/user/purchase", function (req, res) {
+  res.json({
+    message: "signup endpoint",
+  });
+});
+app.post("/course/puchase", function (req, res) {
+  res.json({
+    message: "signup endpoint",
+  });
+});
+app.get("/course", function (req, res) {
+  res.json({
+    message: "signup endpoint",
+  });
+});
